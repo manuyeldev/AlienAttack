c10756e7685d4657a145e45c43a5c47cf81798d2
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-28 17:45:43 -0500

    45. Enemy Deathzone

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+var enemy_scene = preload("res://scenes/enemy.tscn")
+
+@onready var spawn_positions = $SpawnPositions
 
 func _on_timer_timeout():
-	print("Timeout")
+	spawn_enemy()
+	
+func spawn_enemy():
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	
+	var enemy_instance = enemy_scene.instantiate()
+	enemy_instance.global_position = random_spawn_position.global_position
+	add_child(enemy_instance)
--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -4,6 +4,8 @@ extends Area2D
 
 @onready var visible_notifier = $VisibleNotifier
 
+var enemy_scene = preload("res://scenes/enemy.tscn")
+
 func _ready():
 	visible_notifier.connect("screen_exited", _on_screen_exited)
 
@@ -12,3 +14,8 @@ func _physics_process(delta):
 	
 func _on_screen_exited():
 	queue_free()
+
+func _on_area_entered(area):
+	queue_free()
+	area.die()
+	
