a79d6fcb48c504954a10223d71b31f4bbcd6c92b
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-28 20:34:50 -0500

    47. Scoring & Enemy Died Signal

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var lives = 3
+var score = 0
 
 @onready var player = $Player
 
@@ -14,3 +15,11 @@ func _on_player_took_damage():
 		player.die()
 	else:
 		print(lives)
+
+func _on_enemy_spawner_enemy_spawned(enemy_instance):
+	enemy_instance.connect("died", _on_enemy_died)
+	add_child(enemy_instance)
+
+func _on_enemy_died():
+	score += 100
+	print("Score: " + str(score))
