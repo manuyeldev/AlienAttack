6caadb58988c87cbfab3df1cacb41fc44ccd77f2
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-29 08:48:24 -0500

    50. Game Over Screen

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,20 +5,28 @@ var score = 0
 
 @onready var player = $Player
 @onready var hud = $UI/HUD
+@onready var ui = $UI
+
+var gos_scene = preload("res://scenes/game_over_screen.tscn")
 
 func _ready():
 	hud.set_score_label(0)
 	hud.set_lives(lives)
 
 func _on_death_zone_area_entered(area):
-	area.die()
+	area.queue_free()
 
 func _on_player_took_damage():
 	lives -= 1
 	hud.set_lives(lives)
 	if (lives == 0):
-		print("Game Over")
 		player.die()
+		
+		await  get_tree().create_timer(1.5).timeout
+		
+		var gos = gos_scene.instantiate()
+		gos.set_score(score)
+		ui.add_child(gos)
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	enemy_instance.connect("died", _on_enemy_died)
