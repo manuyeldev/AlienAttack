cf18fdd2cdc2ac0e1e7c03966234506e5bc2c66d
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-29 09:03:08 -0500

    51. Flame Particle Effect

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,6 +7,9 @@ var score = 0
 @onready var hud = $UI/HUD
 @onready var ui = $UI
 
+@onready var enemy_hit_sound = $EnemyHitSound
+@onready var player_hit_sound = $PlayerHitSound
+
 var gos_scene = preload("res://scenes/game_over_screen.tscn")
 
 func _ready():
@@ -17,6 +20,7 @@ func _on_death_zone_area_entered(area):
 	area.queue_free()
 
 func _on_player_took_damage():
+	player_hit_sound.play()
 	lives -= 1
 	hud.set_lives(lives)
 	if (lives == 0):
@@ -35,3 +39,4 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 func _on_enemy_died():
 	score += 100
 	hud.set_score_label(score)
+	enemy_hit_sound.play()
