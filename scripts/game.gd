02e6ead1a5d3fffb756de4557010b92b15d06b4c
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-28 21:11:31 -0500

    48. Score UI

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,6 +4,10 @@ var lives = 3
 var score = 0
 
 @onready var player = $Player
+@onready var hud = $UI/HUD
+
+func _ready():
+	hud.set_score_label(0)
 
 func _on_death_zone_area_entered(area):
 	area.die()
@@ -22,4 +26,4 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 
 func _on_enemy_died():
 	score += 100
-	print("Score: " + str(score))
+	hud.set_score_label(0)
---
+++ scripts/hud.gd
@@ -0,0 +1,7 @@
+extends Control
+
+@onready var score = $Score
+
+func set_score_label(new_score):
+	score.text = "SCORE: " + str(new_score)
+
